791a62414106c4e4b8f17db178c4360e3fd2bbb8
Lucas <imcvlucas@gmail.com>
2022-10-14 02:43:55 -0400

    fix: added requirements.txt and whitenoise

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-14h)*tahaxjacng25y=v=1*x5=irrni(ycegl@+h&l7@dacs=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,12 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'weather',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
