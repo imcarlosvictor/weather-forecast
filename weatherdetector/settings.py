235acfff9e98521536bd67ab4722da78757a80ac
Lucas <imcvlucas@gmail.com>
2022-10-25 16:55:17 -0400

    turned debug off

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-14h)*tahaxjacng25y=v=1*x5=irrni(ycegl@+h&l7@dacs=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
