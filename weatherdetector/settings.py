9267bc6e8e532643258ca3d65945c88c7920edef
Lucas <imcvlucas@gmail.com>
2023-11-17 10:00:10 -0500

    updated settings for vercel deployment

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-14h)*tahaxjacng25y=v=1*x5=irrni(ycegl@+h&l7@dacs=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
